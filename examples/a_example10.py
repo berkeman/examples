9bbc85841d08f114c368e8438b606a5c4831747f
Anders Berkeman <anders@berkeman.org>
2019-11-18 21:37:37 +0100

    [example1[01]] comments

--- examples/a_example10.py
+++ examples/a_example10.py
@@ -1,9 +1,11 @@
 def prepare(job):
-	print(dir(job))
+	# Create a datasetwriter with one column
+	# named 'test' of type 'number'.
 	dw = job.datasetwriter()
 	dw.add('test', 'number')
 	return dw
 
 def analysis(sliceno, prepare_res):
 	dw = prepare_res
+	# In this slice, write the sliceno to the dataset
 	dw.write(sliceno)
--- examples/a_example11.py
+++ examples/a_example11.py
@@ -1,12 +1,16 @@
-
 datasets = ('parent',)
 
 def prepare(job):
+	# Create a datasetwriter with one column named 'strtest' of type
+	# 'unicode'.  This dataset has the source dataset as parent,
+	# i.e. this dataset adds columns to the parent dataset.
 	dw = job.datasetwriter(parent=datasets.parent)
 	dw.add('strtest', 'unicode')
 	return dw
 
 def analysis(sliceno, prepare_res):
 	dw = prepare_res
+	# Read each value in the 'test' column and write as
+	# strings to the 'strtest' column
 	for test in datasets.parent.iterate(sliceno, 'test'):
 		dw.write(str(test))
