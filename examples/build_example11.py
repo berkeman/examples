ac09165ba089dd6b683ea094d03067ed8418233a
Anders Berkeman <anders@berkeman.org>
2019-11-18 21:44:00 +0100

    +build_example11

--- examples/build_example11.py
+++ examples/build_example11.py
@@ -1,12 +1,12 @@
 def main(urd):
 
 	# First, create a new dataset.
-	# Second, add a new column to the first dataset.
+	# Second, create a second dataset chained to the first
 
 	print('\n# Run a method that creates a dataset')
 	job = urd.build('example10')
 	print('# (Now, test "bd dsinfo %s" to inspect the dataset!)' % (job,))
 
-	print('\n# Run a method that appends a column to the dataset')
-	job = urd.build('example11', datasets=dict(parent=job))
+	print('\n# Run a method that creates a datasets and chains it to the previous one.')
+	job = urd.build('example12', datasets=dict(previous=job))
 	print('# (Now, test "bd dsinfo %s" to inspect the dataset!)' % (job,))
