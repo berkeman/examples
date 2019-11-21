cee944c35ef3c7e75ce4502ca12324b9dd76c17c
Anders Berkeman <anders.berkeman@ericsson.com>
2019-11-21 08:38:11 +0100

    [example3] use .output()

--- examples/a_example3.py
+++ examples/a_example3.py
@@ -5,7 +5,7 @@ def synthesis(job):
 	res2 = jobids.second.load()
 	s = 'Hello from \"%s\"' % (job.method,)
 	s += '\n  second job is \"%s\"' % (jobids.second.method,)
-	s += '\n  first job took %f seconds' % (jobids.first.post().profile.total,)
+	s += '\n  first job took %f seconds' % (jobids.first.post.profile.total,)
 	s += '\n  now I store something in \"ex3file\"'
 	job.save(dict(a=3, b='foo'), 'ex3file')
 	return s
